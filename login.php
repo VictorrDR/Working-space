527ed3c425f0d7f116dac9ec6a0dcc312b2f4bf1
VictorrDR <146172390+VictorrDR@users.noreply.github.com>
2023-10-13 15:00:53 +0100

    Update and rename login.css to login.php

---
+++ login.php
@@ -0,0 +1,17 @@
+<?php
+// Define your secret password
+$secretPassword = "your_password_here";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $enteredPassword = $_POST["password"];
+    if ($enteredPassword == $secretPassword) {
+        // Password is correct, grant access to your website
+        header("Location: your_website_homepage.html");
+        exit();
+    } else {
+        // Password is incorrect, redirect to the login page
+        header("Location: login.html");
+        exit();
+    }
+}
+?>
